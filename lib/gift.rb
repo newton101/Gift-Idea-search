70c56d539a7a65cabe4919ce8b1a90903999dfb7
Aouled Miguil <miguil@hotmail.co.uk>
2013-06-13 12:04:50 +0100

    refactor the code further

--- lib/gift.rb
+++ lib/gift.rb
@@ -1,11 +1,37 @@
 require 'json'
 class Gift
 
-  def self.results_for(title)
-  	json_data = '{"gift_ideas":[{"gift_idea":{"title":"Porsche 911 Carerra S","id":null,"type":"car"}},{"gift_idea":{"title":"Porsche 911 Carerra S","id":2,"type":"car"}},{"gift_idea":{"title":"Hugo_boss","id":null,"type":"handbag"}}]}'
-  	parsed = JSON.parse(json_data)
-    results = parsed["gift_ideas"].select do|gift| 
-    	gift["gift_idea"]["title"] == title && gift["gift_idea"]["id"] == nil 
+  def self.find_by_title title
+    filtered_results_by title
+  end
+
+  def self.parse_json json
+    parsed = JSON.parse json 
+    parsed
+  end
+
+  def self.check_for_title gift, title
+  	gift["gift_idea"]["title"] == title
+  end
+
+  def self.check_id_is_null gift
+    gift["gift_idea"]["id"] == nil
+  end
+
+  def self.filtered_results_by title
+  	json_collection = parse_json json_data 
+    filter_all_by_title_and_id json_collection, title
+  end
+
+  private
+
+  def self.json_data
+    '{"gift_ideas":[{"gift_idea":{"title":"Porsche 911 Carerra S","id":null,"type":"car"}},{"gift_idea":{"title":"Porsche 911 Carerra S","id":2,"type":"car"}},{"gift_idea":{"title":"Hugo_boss","id":null,"type":"handbag"}}]}'  
+  end
+
+   def self.filter_all_by_title_and_id gifts_collection, title
+    gifts_collection["gift_ideas"].select do|gift| 
+      check_for_title(gift, title) &&  check_id_is_null(gift)
     end
   end
 end
